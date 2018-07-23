251a9778796f3037cab185c780635f65e85988a8
Daniela Lemow <danielalemow@danielas-mbp.student.uod.otago.ac.nz>
2018-07-24 11:17:10 +1200

    Committing initial main method.

---
+++ prog4.1/prog4.1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  prog4.1
+//
+//  Created by Daniela Lemow on 24/07/18.
+//  Copyright © 2018 Daniela Lemow. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
